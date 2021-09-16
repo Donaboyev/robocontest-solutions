54edb422b8d3e9010e7cde1a675ef19e353ca007
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-09-16 22:20:10 +0500

    problem 0058 solved

---
+++ problem0058.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    switch (a){
+    case 1:
+        cout << 6;
+        break;
+    case 2:
+        cout << 5;
+        break;
+    case 3:
+        cout << 4;
+        break;
+    case 4:
+        cout << 3;
+        break;
+    case 5:
+        cout << 2;
+        break;
+    case 6:
+        cout << 1;
+        break;
+    }
+    return 0;
+}
