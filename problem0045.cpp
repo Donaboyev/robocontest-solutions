09f609bf810c90353329d8a347d5f52f4f079e35
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-12-08 11:10:47 +0500

    problem 0045 solved

---
+++ problem0045.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+    cout << (n * (n + 1) / 2);
+
+    return 0;
+}
