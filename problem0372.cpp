8ba7d6940bff5b06383ee50b99b1bcd663364779
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-12-08 12:49:30 +0500

    problem 0372 solved

---
+++ problem0372.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    cout << ((a >= b && a >= c) ? a : (b >= c ? b : c));
+
+    return 0;
+}
